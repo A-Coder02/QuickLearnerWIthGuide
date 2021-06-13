9f9afb85fc60f4369a4a707fc3111f2f56b05df8
A-Coder02 <65939474+A-Coder02@users.noreply.github.com>
2021-06-13 12:34:17 +0530

    Add files via upload

---
+++ topics/ELECT/pages/script/pages.js
@@ -0,0 +1,40 @@
+function functi(){
+
+document.getElementById('demo1').style.width='220px';
+document.getElementById('demo').style.filter='opacity(1.0)';
+
+
+document.getElementById('bu2').style.left='180px';
+document.getElementById('bu2').style.filter='opacity(1.9)';
+document.getElementById('bu1').style.filter='opacity(0.0)';
+
+<!-- document.getElementById('bu1').style.display='none'; -->
+
+}
+function functiR(){
+
+document.getElementById('demo1').style.width='0px';
+document.getElementById('demo').style.filter='opacity(0.0)';
+document.getElementById('bu2').style.filter='opacity(0.0)';
+document.getElementById('bu1').style.filter='opacity(1.0)';
+document.getElementById('bu2').style.left='0px';
+<!-- document.getElementById('bu1').style.display='block'; -->
+}
+
+function  functiHS(){
+document.getElementById('subjects').style.top="30px";
+document.getElementById('last').style.display="none";
+document.getElementById('last1').style.display="block";
+
+
+}
+function  functiHS1(){
+document.getElementById('subjects').style.top="-150px";
+document.getElementById('last').style.display="block";
+document.getElementById('last1').style.display="none";
+
+
+}
+
+<!-- document.getElementById('bu1').style.display='block'; -->
+
